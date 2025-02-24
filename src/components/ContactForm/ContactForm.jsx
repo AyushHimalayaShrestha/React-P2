dcf38168018edd28257e06b4bd046df69b5d14a2
Ayush  Shrestha <ayushshrestha@Ayushs-MacBook-Pro.local>
2025-02-24 23:09:39 +0545

    Button Icon

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import styles from './Contact.module.css';
+import Button from '../Button/Button';
 const ContactForm = () => {
   return (
     <section className={styles.container}>
-        <div className={styles.contact_form}></div>
+        <div className={styles.contact_form}>
+          <Button/>
+        </div>
+
         <div className={styles.contact_image}></div>
     </section>
   )
