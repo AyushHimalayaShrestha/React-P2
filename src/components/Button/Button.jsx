69513c03f077c6c8a902961c0545046163cbe978
Ayush  Shrestha <aayush625@gmail.com>
2025-02-25 15:48:47 +0545

    button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { MdMessage } from "react-icons/md";
+import styles from './Button.module.css';
 const Button = () => {
   return (
-    <div>
-        <MdMessage />
-    </div>
+    <button className={styles.primary_btn}>
+        <MdMessage  fontSize='24px'/>
+        VIA SUPPORT CHAT
+    </button>
   )
 }
 
