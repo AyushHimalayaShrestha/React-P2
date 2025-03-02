ac32f3a54154f25cc1cd5bb4b4a1e6c96a1bf49a
Ayush  Shrestha <aayush625@gmail.com>
2025-03-02 21:31:46 +0545

    button event

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -6,12 +6,18 @@ import { FaPhoneAlt } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
 const ContactForm = () => {
+
+  const onViaCallSubmit =()=>{
+    console.log('Call')
+  }
   return (
     <section className={styles.container}>
         <div className={styles.contact_form}>
           <div className={styles.top_btn}> 
           <Button text='VIA SUPPORT CHAT' icon={<MdMessage fontSize='24px'/>} />
-          <Button text='VIA Call' icon={<FaPhoneAlt fontSize='24px'/>} />
+          <Button
+          onClick={onViaCallSubmit}
+          text='VIA Call' icon={<FaPhoneAlt fontSize='24px'/>} />
           </div>
           <Button 
           isOutline={true}
